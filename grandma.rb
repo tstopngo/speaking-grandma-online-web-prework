4c7606f8b4a05bec32d43af6bf6319e79b3846ab
Tracy Ngo <tngo1206@gmail.com>
2019-02-03 23:24:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase()
-    return "NO, NOT SINCE 1939!"
+    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
